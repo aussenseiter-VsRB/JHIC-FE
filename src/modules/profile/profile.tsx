f4b7f75f077724886a424aa1c225eda7ee545903
RikshaJiwanaSinatrya <sinatryariksha@gmail.com>
2026-07-17 14:06:34 +0700

    chore : sambutan

--- src/modules/profile/profile.tsx
+++ src/modules/profile/profile.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import TentangSekolah from "./components/TentangSekolah";
+import SambutanKepsek from "./components/SambutanKepsek";
 import "./css/profile.css";
 
 function Profile() {
@@ -13,7 +14,11 @@ function Profile() {
 >>>>>>> 63a210a (feat : nambah routes dan home)
 =======
       <TentangSekolah />
+<<<<<<< HEAD
 >>>>>>> 3806e20 (chore : hero section in profile)
+=======
+      <SambutanKepsek />
+>>>>>>> 4535005 (chore : sambutan)
       <Outlet />
     </div>
   );
