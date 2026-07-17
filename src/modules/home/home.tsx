af5184298d519814e2c2a8c04c5ce6fe00ead4a6
RikshaJiwanaSinatrya <sinatryariksha@gmail.com>
2026-07-17 08:12:03 +0700

    feat : nambah routes dan home

--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -1,8 +1,15 @@
 import "./css/home.css";
+import Hero from "./components/Hero";
 
 function Home() {
   return (
+<<<<<<< HEAD
 <div className="div"></div>   
+=======
+    <div className="home">
+      <Hero />
+    </div>
+>>>>>>> 63a210a (feat : nambah routes dan home)
   );
 }
 
--- src/modules/profile/profile.tsx
+++ src/modules/profile/profile.tsx
@@ -1,10 +1,15 @@
 import { Outlet } from "react-router-dom";
+import ProfileCard from "./components/ProfileCard";
 import "./css/profile.css";
 
 function Profile() {
   return (
     <div className="profile">
+<<<<<<< HEAD
       
+=======
+      <ProfileCard />
+>>>>>>> 63a210a (feat : nambah routes dan home)
       <Outlet />
     </div>
   );
