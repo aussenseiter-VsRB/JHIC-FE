66655e8183ef49dba6dbdbf0fb8057540a35116f
RikshaJiwanaSinatrya <sinatryariksha@gmail.com>
2026-07-17 08:12:03 +0700

    feat : nambah routes dan home

--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -1,20 +1,11 @@
+import "./css/home.css";
 import Hero from "./components/Hero";
 
 function Home() {
   return (
-<<<<<<< HEAD
-<<<<<<< HEAD
-<div className="div"></div>   
-=======
     <div className="home">
       <Hero />
     </div>
->>>>>>> 63a210a (feat : nambah routes dan home)
-=======
-    <div>
-      <Hero />
-    </div>
->>>>>>> 100c835 (chore: header section)
   );
 }
 
--- src/modules/profile/profile.tsx
+++ src/modules/profile/profile.tsx
@@ -1,24 +1,15 @@
 import { Outlet } from "react-router-dom";
 import TentangSekolah from "./components/TentangSekolah";
 import SambutanKepsek from "./components/SambutanKepsek";
+import ProfileCard from "./components/ProfileCard";
 import "./css/profile.css";
 
 function Profile() {
   return (
     <div className="profile">
-<<<<<<< HEAD
-<<<<<<< HEAD
-      
-=======
       <ProfileCard />
->>>>>>> 63a210a (feat : nambah routes dan home)
-=======
       <TentangSekolah />
-<<<<<<< HEAD
->>>>>>> 3806e20 (chore : hero section in profile)
-=======
       <SambutanKepsek />
->>>>>>> 4535005 (chore : sambutan)
       <Outlet />
     </div>
   );
