08abc9a1b58d04e3f6e59f0847e42f4f0efe76fb
RikshaJiwanaSinatrya <sinatryariksha@gmail.com>
2026-07-17 13:28:07 +0700

    chore : hero section in profile

--- src/modules/profile/profile.tsx
+++ src/modules/profile/profile.tsx
@@ -1,14 +1,19 @@
 import { Outlet } from "react-router-dom";
+import TentangSekolah from "./components/TentangSekolah";
 import "./css/profile.css";
 
 function Profile() {
   return (
     <div className="profile">
 <<<<<<< HEAD
+<<<<<<< HEAD
       
 =======
       <ProfileCard />
 >>>>>>> 63a210a (feat : nambah routes dan home)
+=======
+      <TentangSekolah />
+>>>>>>> 3806e20 (chore : hero section in profile)
       <Outlet />
     </div>
   );
