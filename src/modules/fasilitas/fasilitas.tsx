08cf1ea8bda85401c616cd99db2669de04bdf618
max-tantan <fatanjamterbang@gmail.com>
2026-07-20 11:25:34 +0700

    feat: add berita and fasilitas modules with basic structure and styles

---
+++ src/modules/fasilitas/fasilitas.tsx
@@ -0,0 +1,11 @@
+import "./css/fasilitas.css";
+
+function Fasilitas() {
+  return (
+    <div className="fasilitas">
+      <h1>Fasilitas</h1>
+    </div>
+  );
+}
+
+export default Fasilitas;
---
+++ src/modules/ppdb/ppdb.tsx
@@ -0,0 +1,11 @@
+import "./css/ppdb.css";
+
+function Ppdb() {
+  return (
+    <div className="ppdb">
+      <h1>Ppdb</h1>
+    </div>
+  );
+}
+
+export default Ppdb;
