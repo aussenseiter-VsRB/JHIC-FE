587600911c94f5cbefae8fe84a48d4aecd389367
RikshaJiwanaSinatrya <sinatryariksha@gmail.com>
2026-07-22 13:51:23 +0700

    feat :  wave header

--- src/modules/profile/components/ProfileCard.tsx
+++ src/modules/profile/components/ProfileCard.tsx
@@ -49,8 +49,12 @@ function ProfileCard() {
         </div>
       </div>
 
-      {/* Diagonal bottom edge */}
-      <div className="absolute bottom-0 left-0 right-0 h-20 bg-pearl" style={{ clipPath: 'polygon(0 60%, 100% 0, 100% 100%, 0 100%)' }} />
+      {/* Animated wave bottom edge */}
+      <div className="absolute bottom-0 left-0 right-0 h-20 overflow-hidden">
+        <svg className="wave-scroll" viewBox="0 0 2880 120" fill="none" preserveAspectRatio="none" style={{ width: '200%', height: '80px' }}>
+          <path d="M0,80 C320,120 420,40 720,80 C1020,120 1120,40 1440,80 C1760,120 1860,40 2160,80 C2460,120 2560,40 2880,80 L2880,120 L0,120 Z" fill="#F5F5F5" />
+        </svg>
+      </div>
     </section>
   );
 }
