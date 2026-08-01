9a43c8e128fb27c4db8bce26cdeea9b885df1706
RikshaJiwanaSinatrya <sinatryariksha@gmail.com>
2026-08-01 15:37:30 +0700

    refactor: remove MyJurusan route and component import from application routing

--- src/core/routes.tsx
+++ src/core/routes.tsx
@@ -12,7 +12,6 @@ import FasilitasJurusan from "../modules/fasilitas-jurusan/fasilitas-jurusan";
 import Ppdb from "../modules/ppdb/ppdb";
 import Ekstrakurikuler from "../modules/ekstrakurikuler/ekstrakurikuler";
 import EkstrakurikulerDetail from "../modules/ekstrakurikuler/ekstrakurikuler-detail/page";
-import MyJurusan from "../modules/myJurusan/myJurusan";
 import NexxaMatch from "../modules/nexxa-match/nexxa-match";
 import Profilguru from "../modules/profilguru/profilguru";
 import Hubin from "../modules/hubin/hubin";
@@ -41,7 +40,6 @@ const routes: RouteObject[] = [
       { path: "/ppdb", element: <Ppdb /> },
       { path: "/ekstrakurikuler", element: <Ekstrakurikuler /> },
       { path: "/ekstrakurikuler/:slug", element: <EkstrakurikulerDetail /> },
-      { path: "/my-jurusan", element: <MyJurusan /> },
       { path: "/nexxa-match", element: <NexxaMatch /> },
       { path: "/profilguru", element: <Profilguru /> },
       { path: "/hubin", element: <Hubin /> },
